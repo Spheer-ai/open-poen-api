ef34d52d512b1b3f3c87f2941fc5d68a03a2e612
Mark de Wijk <dewijkmark@gmail.com>
2023-07-24 11:34:20 +0200

    This solution with association_proxies would be very nice, but it's not working so far...

--- open_poen_api/schemas_and_models/models/entities.py
+++ open_poen_api/schemas_and_models/models/entities.py
@@ -17,6 +17,7 @@ from sqlalchemy_utils import ChoiceType
 # from ..mixins import TimeStampMixin, HiddenMixin, Money
 from typing import Optional
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
 from fastapi_users.db import SQLAlchemyBaseUserTable
 
 
@@ -24,19 +25,36 @@ class Base(DeclarativeBase):
     pass
 
 
-user_initiative = Table(
-    "user_initiative",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("user.id")),
-    Column("initiative_id", Integer, ForeignKey("initiative.id")),
-)
+# user_initiative = Table(
+#     "user_initiative",
+#     Base.metadata,
+#     Column("user_id", Integer, ForeignKey("user.id")),
+#     Column("initiative_id", Integer, ForeignKey("initiative.id")),
+# )
 
-user_activity = Table(
-    "user_activity",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("user.id")),
-    Column("activity_id", Integer, ForeignKey("activity.id")),
-)
+
+class UserInitiativeRole(Base):
+    __tablename__ = "user_initiative_roles"
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    initiative_id = Column(Integer, ForeignKey("initiative.id"), primary_key=True)
+    user = relationship("User", back_populates="initiative_roles")
+    initiative = relationship("Initiative", back_populates="user_roles")
+
+
+# user_activity = Table(
+#     "user_activity",
+#     Base.metadata,
+#     Column("user_id", Integer, ForeignKey("user.id")),
+#     Column("activity_id", Integer, ForeignKey("activity.id")),
+# )
+
+
+class UserActivityRole(Base):
+    __tablename__ = "user_activity_roles"
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    activity_id = Column(Integer, ForeignKey("activity.id"), primary_key=True)
+    user = relationship("User", back_populates="activity_roles")
+    activity = relationship("Activity", back_populates="user_roles")
 
 
 class Role(str, Enum):
@@ -59,18 +77,18 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     hidden: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
     bng = relationship("BNG", uselist=False, back_populates="user", lazy="selectin")
-    initiatives = relationship(
-        "Initiative",
-        secondary=user_initiative,
-        back_populates="initiative_owners",
+    initiative_roles = relationship(
+        "UserInitiativeRole",
+        back_populates="user",
         lazy="selectin",
     )
-    activities = relationship(
-        "Activity",
-        secondary=user_activity,
-        back_populates="activity_owners",
+    initiatives = association_proxy("initiative_roles", "initiative")
+    activity_roles = relationship(
+        "UserActivityRole",
+        back_populates="user",
         lazy="selectin",
     )
+    activities = association_proxy("activity_roles", "activity")
 
     def __repr__(self):
         return f"User(id={self.id}, name='{self.first_name} {self.last_name}', role='{self.role}', is_superuser='{self.is_superuser}')"
@@ -129,12 +147,12 @@ class Initiative(Base):
     )
     hidden: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
-    initiative_owners = relationship(
-        "User",
-        secondary=user_initiative,
-        back_populates="initiatives",
-        lazy="selectin",
+    user_roles = relationship(
+        "UserInitiativeRole",
+        back_populates="initiative",
+        lazy="immediate",
     )
+    initiative_owners = association_proxy("user_roles", "user")
     activities = relationship("Activity", back_populates="initiative", lazy="selectin")
 
     def __repr__(self):
@@ -155,12 +173,12 @@ class Activity(Base):
     finished: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     finished_description: Mapped[str] = mapped_column(String(length=512))
 
-    activity_owners = relationship(
-        "User",
-        secondary=user_activity,
-        back_populates="activities",
+    user_roles = relationship(
+        "UserActivityRole",
+        back_populates="activity",
         lazy="selectin",
     )
+    activity_owners = association_proxy("user_roles", "user")
     initiative_id: Mapped[int] = mapped_column(Integer, ForeignKey("initiative.id"))
     initiative = relationship(
         "Initiative", back_populates="activities", lazy="selectin"
